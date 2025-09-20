bfacbcd5ac95e0d96e31564e5ab940cde825e07a
Steve <contact@stevedylan.dev>
2025-09-20 15:45:14 -0400

    chore: updated to use tailwindcss for site

--- scripts/deploy-site.ts
+++ scripts/deploy-site.ts
@@ -1,25 +1,28 @@
 import { $ } from "bun";
-// Copy components folder from src to site
-await $`cp -r src/components site/components`;
 
-// Update script tag in HTML file to use local components path
-const htmlContent = await Bun.file("site/index.html").text();
-const updatedHtml = htmlContent.replace(
-	`<script src="../src/components/connect-wallet.js"></script>`,
-	`<script src="components/connect-wallet.js"></script>`,
-);
-await Bun.write("site/index.html", updatedHtml);
+// Clean up old dist
+await $`rm -rf site-dist`;
+// Create new dist
+await $`cp -r site site-dist`;
+// Compile tailwindcss
+await $`bunx @tailwindcss/cli -i ./site/input.css -o ./site-dist/output.css `;
+// Copy components over
+await $`cp -r src/components site-dist/components`;
+
+// Read index file
+const htmlContent = await Bun.file("site-dist/index.html").text();
+// Update script tags and css link
+const updatedHtml = htmlContent
+	.replace(
+		`<script src="../src/components/connect-wallet.js"></script>`,
+		`<script src="components/connect-wallet.js"></script>`,
+	)
+	.replace(
+		`<link rel="stylesheet" href="tailwindcss" />`,
+		`<link rel="stylesheet" href="output.css" />`,
+	);
+// Write file
+await Bun.write("site-dist/index.html", updatedHtml);
 
 // Run orbiter update
-await $`orbiter update -d norns site`;
-
-// Remove components folder from site
-await $`rm -rf site/components`;
-
-// Change script tag back to original path
-const finalHtmlContent = await Bun.file("site/index.html").text();
-const restoredHtml = finalHtmlContent.replace(
-	`<script src="components/connect-wallet.js"></script>`,
-	`<script src="../src/components/connect-wallet.js"></script>`,
-);
-await Bun.write("site/index.html", restoredHtml);
+await $`orbiter update -d norns site-dist`;
